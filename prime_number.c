2d462aef1e54b83e838c712ed24dd3d44ea6ddde
hemangsrivastava <hemangsrivastava.contact@gmail.com>
2026-01-21 16:58:03 +0530

    Add files via upload

---
+++ prime_number.c
@@ -0,0 +1,15 @@
+/*Write a program to check if a number is prime.*/
+#include <stdio.h>
+int main(){
+int a,i,c=0; 
+printf("enter the number: ");
+scanf("%d",&a);
+for (i=1;i<=a;i++){
+if (a % i ==0){
+c++;}}
+if (c==2){
+printf("Prime Number");}
+else{
+printf("Not Prime Number");}
+return 0;
+}
